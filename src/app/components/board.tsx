0d209bdf760919e3267ca311aecb79a18bf362ae
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-18 01:44:13 -0300

    Create Winner Checker

--- src/app/components/board.tsx
+++ src/app/components/board.tsx
@@ -15,6 +15,76 @@ for (let i = 0; i < COLS; i += 1) {
   }
 }
 
+type Direction =
+  | "up"
+  | "down"
+  | "left"
+  | "right"
+  | "upLeft"
+  | "upRight"
+  | "downLeft"
+  | "downRight";
+
+const whoWon = (slots: ("1" | "2" | null)[][]): "1" | "2" | null => {
+  for (let i = 0; i < COLS; i += 1) {
+    for (let j = 0; j < ROWS; j += 1) {
+      const player = slots[i][j];
+      if (player === null) continue;
+
+      const winMatcher = (
+        player: "1" | "2",
+        i: number,
+        j: number,
+        count = 1,
+        direction?: Direction
+      ): "1" | "2" | null => {
+        if (player !== slots[i]?.[j]) return null;
+        if (count === 4) return player;
+
+        const newCount = count + 1;
+
+        const directions = {
+          left: [i - 1, j],
+          right: [i + 1, j],
+          up: [i, j - 1],
+          down: [i, j + 1],
+          upLeft: [i - 1, j - 1],
+          upRight: [i + 1, j - 1],
+          downLeft: [i - 1, j + 1],
+          downRight: [i + 1, j + 1],
+        } as const;
+
+        if (count === 1) {
+          return Object.entries(directions).reduce<"1" | "2" | null>(
+            (winner, [directionName, direction]) =>
+              winner ||
+              winMatcher(
+                player,
+                direction[0],
+                direction[1],
+                newCount,
+                directionName as Direction
+              ),
+            null
+          );
+        }
+
+        return winMatcher(
+          player,
+          directions[direction!][0],
+          directions[direction!][1],
+          newCount,
+          direction!
+        );
+      };
+
+      const winner = winMatcher(player, i, j);
+      if (winner !== null) return winner;
+    }
+  }
+  return null;
+};
+
 type PieceProps = {
   player: "1" | "2" | null;
   onClick?: () => void;
@@ -44,6 +114,8 @@ export default function Board() {
     });
     changePlayer();
   };
+
+  console.log("Who Won? ", whoWon(gameSlots));
 
   const isColFull = (colNumber: number) =>
     gameSlots[colNumber].lastIndexOf(null) === -1;
