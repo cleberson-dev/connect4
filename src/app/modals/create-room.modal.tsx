939ce458c64c91956d27ff7db236e1cdaaddf6be
Cleberson Junior <cleberson.dev@gmail.com>
2024-01-30 07:57:58 -0300

    Adjust create room form

--- src/app/modals/create-room.modal.tsx
+++ src/app/modals/create-room.modal.tsx
@@ -9,31 +9,47 @@ export default function CreateRoomModal({ onCreate }: CreateRoomModalProps) {
     register,
     formState: { isValid },
     handleSubmit,
-  } = useForm<{ name: string; password: string }>();
+  } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      roomName: "",
+      roomPassword: "",
+    },
+  });
 
   const onSubmit: Parameters<typeof handleSubmit>[0] = (values) => {
-    const { name, password } = values;
-    onCreate(name, password);
+    const { roomName, roomPassword } = values;
+    onCreate(roomName, roomPassword);
   };
 
   return (
     <form
       className="bg-white p-8 shadow-sm rounded flex flex-col gap-y-2"
+      autoComplete="off"
       onSubmit={handleSubmit(onSubmit)}
     >
       <input
         className="p-2 rounded border border-solid"
         placeholder="Your Room Name"
-        {...register("name", { min: 4, max: 16, required: true })}
+        {...register("roomName", {
+          minLength: 4,
+          maxLength: 16,
+          required: true,
+        })}
       />
       <input
         type="password"
         placeholder="Enter a password for your room"
         className="p-2 rounded border border-solid"
-        {...register("password", { min: 4, max: 10, required: true })}
+        {...register("roomPassword", {
+          minLength: 4,
+          maxLength: 10,
+          required: true,
+        })}
       />
       <button
-        className="p-2 rounded shadow-sm bg-blue-500 text-white"
+        className="p-2 rounded shadow-sm bg-blue-500 text-white disabled:bg-slate-200 disabled:text-black disabled:opacity-50"
+        type="submit"
         disabled={!isValid}
       >
         Create Room
