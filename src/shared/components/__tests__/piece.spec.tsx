a14eada0ae69f08c4f0eb509f91a633eb72d1485
Cleberson Junior <cleberson.dev@gmail.com>
2024-02-13 17:32:15 -0300

    Create some other tests for Piece

--- src/shared/components/__tests__/piece.spec.tsx
+++ src/shared/components/__tests__/piece.spec.tsx
@@ -1,9 +1,40 @@
-import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { expect, test, describe } from "vitest";
+import { render, screen, cleanup } from "@testing-library/react";
 import { Player } from "@/shared/types";
 import Piece from "@/shared/components/piece";
 
-test("<Piece />", () => {
-  render(<Piece player={Player.ONE} />);
-  expect(screen.getByRole("board-piece")).toBeDefined();
+describe("<Piece />", () => {
+  test("should render successfully", () => {
+    cleanup();
+    render(<Piece player={Player.ONE} />);
+    expect(screen.getByRole("board-piece")).toBeDefined();
+  });
+
+  test("should render different elements for empty and non-empty pieces", () => {
+    cleanup();
+    render(
+      <>
+        <Piece player={Player.ONE} />
+        <Piece player={null} />
+      </>
+    );
+
+    const [a, b] = screen.getAllByRole("board-piece");
+
+    expect(a.nodeName).not.toBe(b.nodeName);
+  });
+
+  test("should render same elements for players one and two", () => {
+    cleanup();
+    render(
+      <>
+        <Piece player={Player.ONE} />
+        <Piece player={Player.TWO} />
+      </>
+    );
+
+    const [a, b] = screen.getAllByRole("board-piece");
+
+    expect(a.nodeName).toBe(b.nodeName);
+  });
 });
