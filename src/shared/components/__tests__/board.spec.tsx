308b4219eac1c4241a6c6dc8a3399115e6f4238e
Cleberson Junior <cleberson.dev@gmail.com>
2024-02-13 20:15:29 -0300

    Add two test cases for Board

--- src/shared/components/__tests__/board.spec.tsx
+++ src/shared/components/__tests__/board.spec.tsx
@@ -1,9 +1,42 @@
-import { render, screen } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { beforeEach, expect, test, vi } from "vitest";
+import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import Board from "@/shared/components/board";
 import { Player } from "@/shared/types";
+import { createFreshSlots } from "@/shared/utils";
+
+beforeEach(cleanup);
 
 test("render successfully", () => {
   render(<Board slots={[]} player={Player.ONE} />);
   expect(screen.getByTestId("board")).toBeDefined();
+});
+
+test("call onColumnClick when one of the columns is clicked", async () => {
+  const mock = vi.fn();
+  render(
+    <Board
+      slots={createFreshSlots()}
+      player={Player.ONE}
+      onColumnClick={mock}
+      playable
+    />
+  );
+  await userEvent.click(screen.getByTestId("board").querySelector("div")!);
+  expect(mock).toHaveBeenCalledOnce();
+});
+
+test("not call onColumnClick for not playable boards when one of the columns is clicked", async () => {
+  const mock = vi.fn();
+  render(
+    <Board
+      slots={createFreshSlots()}
+      player={Player.ONE}
+      onColumnClick={mock}
+      playable={false}
+    />
+  );
+  await userEvent.click(screen.getByTestId("board").querySelector("div")!);
+  expect(mock).toHaveBeenCalledOnce();
 });
